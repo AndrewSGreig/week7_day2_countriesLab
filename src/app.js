c3385afd702c70bd240081a7cdb564628ad7ab34
User <user@Users-MacBook-Pro.local>
2020-02-18 14:38:07 +0000

    committing total population

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: '#app',
+    data: {
+      countries: [],
+      selectedCountry: ""
+    },
+    computed: {
+      totalPopulation: function(){
+        return this.countries.reduce((runningTotal, country) => {
+          return runningTotal + country.population
+        }, 0);
+      }
+    },
+    mounted(){
+      this.fetchCountries();
+    },
+    methods: {
+      fetchCountries: function(){
+        const request = fetch('https://restcountries.eu/rest/v2/all')
+        .then(response => response.json())
+        .then(data => this.countries = data)
+    },
+    showCountry: function(){
+
+    }
+  }
+  })
+})
